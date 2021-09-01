5bd84f454c9e5331ae8540e251b6313582046e79
Trzy426 <charleswrogersiii@gmail.com>
2021-09-01 14:40:35 -0700

    made a mistake and added index listing aswell as crud to branch

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,25 +1,29 @@
 class ApartmentsController < ApplicationController
     def index
-        @apartments = Apartment.all
-        render json: @apartments
+        apartments = Apartment.all
+        render json: apartments
     end
 
     def create
-        @apartment = Apartment.create(apartment_params)
+        apartment = Apartment.create(apartment_params)
+        render json: apartment
     end
 
     def update
         apartment = Apartment.find(params[:id])
         apartment.update(apartment_params)
         render json: apartment
+    end
+
     def destroy
         apartment = Apartment.find(params[:id])
         apartment.destroy
         render json:apartment
+    end
     
     private
     def apartment_params
-        params.require(:apartment).permit(:street, :city, :state, :description, :manager, :email, :price:bedrooms, :bathrooms)
-       
+        params.require(:apartment).permit(:street, :city, :state, :description, :manager, :email, :price, :bedrooms, :bathrooms)
+    end
 
 end
--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,11 +1,42 @@
 import React,{Component} from "react"
 
 class ApartmentIndex extends Component {
-    render() {
-        return(
-            <h1>you are at Index</h1>
+    constructor(props){
+        super(props)
+        this.state = {
+            apartments: []
+        }
+    }
+    componentDidMount(){
+        this.apartmentIndex()
+    }
+    apartmentIndex = ()=>{
+        fetch("http://localhost:3000/apartments")
+        .then(response => {
+            return response.json()
+        })
+        .then(apartmentArr =>{
+            this.setState({apartments:apartmentArr})
+            // set state of the empty state array with apartment index
+        })
+        .catch(errors => {
+            console.log("index errors: ", errors)
+        })
+    }
+     render() {
+         console.log("reached render")
+         console.log(this.state.apartments)
+        return (
+            <>
+            {this.state.apartments.map(apartment => {
+                console.log(apartment.street)
+                return <h3>{JSON.stringify(apartment)}</h3>
+            })}
+            </>
         )
     }
 }
 
 export default ApartmentIndex
+
+// {JSON.stringify(apartment)}
